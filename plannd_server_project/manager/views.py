2f96fe338668937d278dd8e1beed2e3fb5ccc424
Tom Fyfe <tomfyfe85@gmail.com>
2024-07-10 18:26:05 +0100

    passed a jason object via index view to the browser

--- plannd_server_project/manager/views.py
+++ plannd_server_project/manager/views.py
@@ -1,8 +1,12 @@
 from django.http import HttpResponse
 
 # from django.shortcuts import render
+import json
+
+# a Python object (dict):
+x = {"name": "John", "age": 30, "city": "New York"}
 
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello World")
+    return HttpResponse(json.dumps(x))
